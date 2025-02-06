372c28ad2a36a6541841e9f9e638fc3baeb8fd80
VadymMakohon <errorks7@gmail.com>
2025-02-06 10:06:43 +0100

    adding and canceling file data

--- script.js
+++ script.js
@@ -5,4 +5,31 @@ messageInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter" && userMessage) {
         console.log(userMessage);
     }
-}); 
+});
+
+fileInput.addEventListener("change", () => {
+    const file = fileInput.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        fileInput.value = "";
+        fileUploadWrapper.querySelector("img").src = e.target.result;
+        fileUploadWrapper.classList.add("file-uploaded");
+        const base64String = e.target.result.split(",")[1];
+
+        // Store file data in userData
+        userData.file = {
+            data: base64String,
+            mime_type: file.type,
+        };
+    };
+
+    reader.readAsDataURL(file);
+});
+
+// Cancel file upload
+fileCancelButton.addEventListener("click", () => {
+    userData.file = {};
+    fileUploadWrapper.classList.remove("file-uploaded");
+});
